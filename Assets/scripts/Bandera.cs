16ee0eb7048d08bf86cec61339bf37bf5caf731a
g30rgina <geokuentaknofunciona@gmail.com>
2026-02-27 13:53:21 +0100

    27

---
+++ Assets/scripts/Bandera.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bandera : MonoBehaviour
+{
+
+    public gamemanager _gameManager;
+    public BoxCollider2D _boxCollider;
+   
+
+
+    void Awake()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<gamemanager>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _boxCollider.enabled = false;
+            StartCoroutine(_gameManager.win());
+        }
+        
+
+    }
+}
